6227682f00103995ca5cef807629b0fbdc43eb1f
XGlitchy30 <antonio.buglione2014@gmail.com>
2025-03-08 16:36:55 +0100

    Whitelist (D/R) and errata updates
    - Updated the whitelists of Retro and Delta
    - Updated the Delta banlist
    - Implemented the new erratum of "Leading the Charge"

--- script/c130000009.lua
+++ script/c130000009.lua
@@ -7,7 +7,7 @@ Scripted by: XGlitchy30
 local s,id,o=GetID()
 Duel.LoadScript("glitchylib_new.lua")
 function s.initial_effect(c)
-	--Tribute 1 Warrior monster; add 1 Level 5 or higher Warrior monster from your Deck to your hand.
+	--Send 1 Warrior monster you control to the GY; add 1 Level 5 or higher Warrior monster from your Deck to your hand.
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(id,0)
 	e1:SetCategory(CATEGORIES_SEARCH)
@@ -19,13 +19,17 @@ function s.initial_effect(c)
 end
 
 --E1
+function s.tgcfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and c:IsAbleToGraveAsCost()
+end
 function s.thfilter(c)
 	return c:IsLevelAbove(5) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsRace,1,false,nil,nil,RACE_WARRIOR) end
-	local g=Duel.SelectReleaseGroupCost(tp,Card.IsRace,1,1,false,nil,nil,RACE_WARRIOR)
-	Duel.Release(g,REASON_COST)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgcfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgcfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -38,3 +42,16 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Search(g)
 	end
 end
+
+--[[
+Pre-Errata Archive
+
+(3/7/2025)
+Activation Cost was "Tribute 1 Warrior monster"
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsRace,1,false,nil,nil,RACE_WARRIOR) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsRace,1,1,false,nil,nil,RACE_WARRIOR)
+	Duel.Release(g,REASON_COST)
+end
+]]
