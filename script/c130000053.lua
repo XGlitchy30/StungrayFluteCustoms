1c95c17fde64c1a5b974e2c0927e82d69ac59588
XGlitchy30 <antonio.buglione2014@gmail.com>
2025-07-14 03:55:31 +0200

    Tag Duel Compatibility update for Sergeant Waves

--- script/c130000053.lua
+++ script/c130000053.lua
@@ -28,6 +28,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--Count cards at the start of the Duel
 	aux.GlobalCheck(s,function()
+		s.NumberOfPlayers = 2
 		local ge=Effect.GlobalEffect()
 		ge:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_CONTINUOUS)
 		ge:SetCode(EVENT_STARTUP)
@@ -38,9 +39,33 @@ end
 local FLAG_STARTED_WITH_60_CARDS_IN_DECK = id
 
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	for p=0,1 do
-		if Duel.GetDeckCount(p)==60 then
-			Duel.RegisterFlagEffect(p,FLAG_STARTED_WITH_60_CARDS_IN_DECK,0,0,0)
+	if Duel.GetPlayersCount(0)*Duel.GetPlayersCount(1)~=1 and not Duel.IsDuelType(DUEL_RELAY) then
+		local pcount=0
+		for p=0,1 do
+			local pct=Duel.GetPlayersCount(p)
+			for i=1,pct do
+				pcount=pcount+1
+				local pid
+				if p==0 then
+					pid = 2*(i-1) + 1
+				else
+					pid = i==1 and 2*pct or 2*(i-1)
+				end
+				if Duel.GetDeckCount(p)==60 then
+					Duel.RegisterFlagEffect(p,FLAG_STARTED_WITH_60_CARDS_IN_DECK,0,0,0,pid)
+				end
+				if pct>1 then
+					Duel.TagSwap(p)
+				end
+			end
+		end
+		s.NumberOfPlayers = pcount
+		
+	else
+		for p=0,1 do
+			if Duel.GetDeckCount(p)==60 then
+				Duel.RegisterFlagEffect(p,FLAG_STARTED_WITH_60_CARDS_IN_DECK,0,0,0)
+			end
 		end
 	end
 end
@@ -59,7 +84,13 @@ end
 
 --E2
 function s.drawcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.PlayerHasFlagEffect(tp,FLAG_STARTED_WITH_60_CARDS_IN_DECK) and e:GetHandler():GetAttackedCount()>0
+	if s.NumberOfPlayers~=2 then
+		local pid = Duel.GetTurnCount()%s.NumberOfPlayers
+		if pid==0 then pid=s.NumberOfPlayers end
+		return Duel.PlayerHasFlagEffectLabel(tp,FLAG_STARTED_WITH_60_CARDS_IN_DECK,pid) and e:GetHandler():GetAttackedCount()>0
+	else
+		return Duel.PlayerHasFlagEffect(tp,FLAG_STARTED_WITH_60_CARDS_IN_DECK) and e:GetHandler():GetAttackedCount()>0
+	end
 end
 function s.drawtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
